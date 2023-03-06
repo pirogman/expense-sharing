d3856d11403cc4290837c19dc3255bc151ad58be
pirogman <pirogman@gmail.com>
2023-03-06 10:18:58 +0200

    Initial Commit

---
+++ Expense Sharing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Expense Sharing
+//
+//  Created by Alex Pirog on 06.03.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Expense Sharing/Expense_SharingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_SharingApp.swift
+//  Expense Sharing
+//
+//  Created by Alex Pirog on 06.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct Expense_SharingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
