ddb211014aa7622443bfca90cc64e476f51bc68f
pirogman <pirogman@gmail.com>
2023-03-10 13:41:17 +0200

    drag fix

--- Expense Sharing/User Profile/UserGroupItemView.swift
+++ Expense Sharing/User Profile/UserGroupItemView.swift
@@ -34,7 +34,7 @@ struct UserGroupItemView: View {
         .lineLimit(1)
         .padding(.vertical, 8)
         .padding(.horizontal, 12)
-        .offset(x: min(offset.width, 0), y: 0)
+        .offset(x: offset, y: 0)
         .contentShape(Rectangle())
         .clipped()
         .onTapGesture {
@@ -50,57 +50,55 @@ struct UserGroupItemView: View {
                     .overlay(alignment: .trailing) {
                         Image(systemName: "trash")
                             .resizable().scaledToFit()
-                            .squareFrame(side: 18)
-                            .padding(.trailing, deleteOnRelease ? abs(offset.width / 2): (keepDeleteDistance - 18) / 2)
+                            .squareFrame(side: trashSize)
+                            .padding(.trailing, deleteOnRelease
+                                     ? abs(dragOffset.width / 2)
+                                     : (keepDeleteDistance - trashSize) / 2)
                             .opacity(showDelete ? 1 : 0)
                             .scaleEffect(deleteScale)
                     }
             }
-            .frame(width: offset.width < 0 ? abs(offset.width) : 0)
+            .frame(width: abs(offset))
         }
         .animation(.interactiveSpring(), value: offset)
         .gesture(
             DragGesture()
-                .onChanged { gesture in
-                    // Drop first change as this gesture may be intercepted by ScrollView
-                    guard firstChangeDropped else {
-                        print("first change")
-                        firstChangeDropped = true
-                        return
-                    }
-                    print("other change")
-                    
-                    offset = gesture.translation
-                    
-                    showDelete = offset.width < -showDeleteDistance
-                    keepDelete = offset.width < -keepDeleteDistance
-                    deleteOnRelease = offset.width < -deleteOnReleaseDistance
-                    
-                    if showDelete {
-                        let scale = (abs(offset.width) - showDeleteDistance) / showDeleteDistance
-                        deleteScale = min(maxDeleteScale, max(scale, minDeleteScale))
-                    } else {
-                        deleteScale = minDeleteScale
-                    }
+                .updating($dragOffset) { value, state, transaction in
+                    state = value.translation
                 }
-                .onEnded { _ in
-                    print("ended")
-                    firstChangeDropped = false
-                    
-                    if deleteOnRelease {
-                        // Do the delete and reset if swiped to much
-                        onDelete()
-                        reset()
-                    } else if keepDelete {
-                        // Keep delete button if swiped enough
-                        offset = CGSize(width: -keepDeleteDistance, height: 0)
-                        deleteScale = maxDeleteScale
-                    } else {
-                        // Hide delete button on incomplete swipe
-                        reset()
-                    }
+                .updating($isDragging) { value, state, translation in
+                    state = true
                 }
         )
+        .onChange(of: dragOffset) { offset in
+            if isDragging {
+                // In process
+                showDelete = offset.width < -showDeleteDistance
+                keepDelete = offset.width < -keepDeleteDistance
+                deleteOnRelease = offset.width < -deleteOnReleaseDistance
+
+                if showDelete {
+                    let scale = (abs(offset.width) - showDeleteDistance) / showDeleteDistance
+                    deleteScale = min(maxDeleteScale, max(scale, minDeleteScale))
+                } else {
+                    deleteScale = minDeleteScale
+                }
+            } else {
+                // Ended
+                if deleteOnRelease {
+                    // Do the delete and reset if swiped to much
+                    onDelete()
+                    reset()
+                } else if keepDelete {
+                    // Keep delete button if swiped enough
+                    deleteOffset = CGSize(width: -keepDeleteDistance, height: 0)
+                    deleteScale = maxDeleteScale
+                } else {
+                    // Hide delete button on incomplete swipe
+                    reset()
+                }
+            }
+        }
     }
     
     // MARK: - Swipe To Delete
@@ -110,16 +108,24 @@ struct UserGroupItemView: View {
     private let deleteOnReleaseDistance: CGFloat = 180
     private let minDeleteScale: CGFloat = 0.25
     private let maxDeleteScale: CGFloat = 1.0
+    private let trashSize: CGFloat = 18
     
-    @State private var firstChangeDropped: Bool = false
-    @State private var offset: CGSize = .zero
+    @GestureState private var dragOffset: CGSize = .zero
+    @GestureState private var isDragging: Bool = false
+    
+    @State private var deleteOffset: CGSize = .zero
     @State private var showDelete: Bool = false
     @State private var keepDelete: Bool = false
     @State private var deleteOnRelease: Bool = false
     @State private var deleteScale: CGFloat = 0.25
     
+    var offset: CGFloat {
+        let width = dragOffset.width < 0 ? abs(dragOffset.width) : abs(deleteOffset.width)
+        return -width
+    }
+    
     private func reset() {
-        offset = .zero
+        deleteOffset = .zero
         showDelete = false
         keepDelete = false
         deleteOnRelease = false
